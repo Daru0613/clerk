4922b97a69b28eced923dc2c11f12d3e0ae47c01
김서진 <nafi6@naver.com>
2025-09-27 15:20:30 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Welcome Mypage!</h1>
+      <p> clerk.com을 이용한 인증</p>
+    </div>
+  )
+}
